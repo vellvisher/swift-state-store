5c840a792ed1d435a1bcad92adfd3df54b3f426c
Vaarnan Drolia <vaarnan@gmail.com>
2023-10-10 19:25:40 +0100

    Update README.md and switch to binary data

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # swift-state-store
+
+### Usage
+
+```swift
+struct MyApp: App {
+  @StateObject private var store = StateStore<AppState>(
+    storeURL: URL.getFileInDocumentsDirectory("state.binpb"))
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+        .environmentObject(store)
+    }
+  }
+}
+
+struct ContentView: View {
+  @EnvironmentObject private var store: StateStore<AppState>
+}
+
+```
+
+
+### ProtoBufs
+
+To use this with an existing protobuf, add the following extension to your state object.
+
+```swift
+extension $ProtoState: StateProtocol {
+  init(serializedData: Data) throws {
+    self = try AppState(serializedData: serializedData)
+  }
+
+  func serialized() throws -> Data {
+    return try self.serializedData()
+  }
+}
+```
--- Sources/swift-state-store/StateStore.swift
+++ Sources/swift-state-store/StateStore.swift
@@ -1,26 +1,28 @@
 import Foundation
 import Logging
 
+/// This protocol must be implemented by a State object that can
+/// be used with the store.
 public protocol StateProtocol {
   init()
-  init(with: String) throws
-  func textFormatString() -> String
+  func serialized() throws -> Data
+  init(serializedData: Data) throws
 }
 
+/// StateStore generalised on a particular State value.
 public actor StateStore<State: StateProtocol>: ObservableObject {
   @MainActor @Published private(set) public var state: State = .init()
 
-  private let storeFile: URL?
+  private let storeURL: URL?
   private let inMemory: Bool
 
-  public init(filename: String?) {
-    if let filename = filename {
-      storeFile = URL.getFileInDocumentsDirectory(filename)
-      inMemory = false
-    } else {
-      storeFile = nil
-      inMemory = true
-    }
+  /// Parameter: storeURL Uses the specified file for persisting
+  ///   the state, otherwise pass nil for in-memory state.
+  /// - Note: Recommended extension for the file is ".binpb"
+  /// https://protobuf.dev/programming-guides/techniques/#suffixes
+  public init(storeURL: URL?) {
+    self.storeURL = storeURL
+    inMemory = storeURL == nil ? true : false
 
     if !inMemory {
       Task { await loadState() }
@@ -28,14 +30,14 @@ public actor StateStore<State: StateProtocol>: ObservableObject {
   }
 
   private func loadState() async {
-    guard let storeFile = storeFile else {
-      Logger().error("Must specify storeFile to load state from")
+    guard let storeURL = storeURL else {
+      Logger().error("Must specify storeURL to load state from")
       return
     }
-    if FileManager.default.fileExists(atPath: storeFile.path) {
+    if FileManager.default.fileExists(atPath: storeURL.path) {
       do {
-        let contents = try String(contentsOfFile: storeFile.path)
-        let persistedState = try State(with: contents)
+        let contents = try Data(contentsOf: storeURL)
+        let persistedState = try State(serializedData: contents)
         await update { _ in
           return persistedState
         }
@@ -45,6 +47,9 @@ public actor StateStore<State: StateProtocol>: ObservableObject {
     }
   }
 
+  /// Update the underlying state to |state|.
+  ///
+  /// - Note: Updates are done on the MainActor.
   public func update(action: (State) -> State) async {
     await MainActor.run {
       Logger().info("Mutating old state: \(state)")
@@ -52,22 +57,24 @@ public actor StateStore<State: StateProtocol>: ObservableObject {
       Logger().info("New state: \(state)")
     }
     await MainActor.run {
+      // Helps notify subscribed SwiftUI views of update.
       self.objectWillChange.send()
     }
     if !self.inMemory {
-      await writeToFile(state.textFormatString())
+      await writeToFile(state)
     }
   }
 
-  private func writeToFile(_ state: String) async {
-    guard let storeFile else {
+  private func writeToFile(_ state: State) async {
+    guard let storeURL else {
       Logger().error("Store filename not specified for writing state")
       return
     }
     do {
-      try state.write(to: storeFile, atomically: true, encoding: String.Encoding.utf8)
+      try state.serialized().write(to: storeURL)
     } catch let error {
-      // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+      // Failed to write file – bad permissions, bad filename, missing permissions,
+      // or it can't be converted to the encoding.
       Logger().error("Error writing: \(error)")
     }
   }
@@ -76,17 +83,5 @@ public actor StateStore<State: StateProtocol>: ObservableObject {
 extension Logger {
   fileprivate init() {
     self.init(label: "StateStore")
-  }
-}
-
-extension URL {
-  fileprivate static func getFileInDocumentsDirectory(_ filename: String) -> URL {
-    if #available(watchOS 9.0, *), #available(iOS 16.0, *), #available(macOS 13.0, *) {
-      return URL.documentsDirectory.appendingPathComponent(filename)
-    }
-
-    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    let documentsDirectory = paths[0]
-    return documentsDirectory.appendingPathComponent(filename)
   }
 }
