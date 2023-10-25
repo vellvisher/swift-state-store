b6ef6f95707c5f98c30a1e527410ce48247d48a9
Vaarnan Drolia <vaarnan@gmail.com>
2023-10-25 15:56:09 +0100

    Add basic test for store

--- Tests/swift-state-storeTests/StateStoreTests.swift
+++ Tests/swift-state-storeTests/StateStoreTests.swift
@@ -1,12 +1,73 @@
+import StateStore
 import XCTest
-@testable import swift_state_store
 
-final class swift_state_storeTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+struct AppState: StateProtocol {
+  // Always starts with 1.
+  var nextRollNumber: Int
+  // [RollNumber: Name]
+  var students: [Int: String]
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+  init() {
+    self.init(nextRollNumber: 1, students: [:])
+  }
+
+  init(nextRollNumber: Int, students: [Int: String]) {
+    self.nextRollNumber = nextRollNumber
+    self.students = students
+  }
+
+  init(from serializedData: Data) throws {
+    let value = serializedData.withUnsafeBytes {
+      $0.load(as: Int.self)
     }
+    self.init(nextRollNumber: value, students: [:])
+  }
+
+  func serialized() throws -> Data {
+    return withUnsafeBytes(of: nextRollNumber) { Data($0) }
+  }
+}
+
+final class StateStoreTest: XCTestCase {
+  let store = StateStore<AppState>(storeURL: nil)
+
+  @MainActor
+  func testUpdateNextRollNumber() async throws {
+    XCTAssertEqual(store.state.nextRollNumber, 1)
+    XCTAssert(store.state.students.isEmpty)
+    await store.update { state in
+      var newState = state
+      newState.nextRollNumber = 2
+      return newState
+    }
+    XCTAssertEqual(store.state.nextRollNumber, 2)
+    XCTAssert(store.state.students.isEmpty)
+  }
+
+  @MainActor
+  func testUpdateStudents() async throws {
+    XCTAssertEqual(store.state.nextRollNumber, 1)
+    XCTAssert(store.state.students.isEmpty)
+    await store.update { state in
+      var newState = state
+      newState.students = [1: "Jenny Dear"]
+      return newState
+    }
+    XCTAssertEqual(store.state.nextRollNumber, 1)
+    XCTAssertEqual(store.state.students[1], "Jenny Dear")
+  }
+
+  @MainActor
+  func testAddNewStudent() async throws {
+    XCTAssertEqual(store.state.nextRollNumber, 1)
+    XCTAssert(store.state.students.isEmpty)
+    await store.update { state in
+      var newState = state
+      newState.nextRollNumber = 2
+      newState.students = [1: "Jenny Dear"]
+      return newState
+    }
+    XCTAssertEqual(store.state.nextRollNumber, 2)
+    XCTAssertEqual(store.state.students[1], "Jenny Dear")
+  }
 }
